aceaf99f28ba8a47e92da2cd7223c9ab5be88ebb
Dmytro14 <60977979+Dmytro14@users.noreply.github.com>
2022-10-19 23:20:27 +0300

    changes_norm

--- App.js
+++ App.js
@@ -1,46 +1,44 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, FlatList} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import ListJourney from './components/ListJourney';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Inputs from './inputs';
 
-export default function App() {
-  const [listOfItems, setListOfItems] = useState([
-    { start: 'Київ', end: 'Львів', time_start: '23:00', name_driver: 'Богдан', phone_number: '+380988560977', places: 3 },
-    { start: 'Гадяч', end: 'Миргород', time_start: '10:00', name_driver: 'Віталік', phone_number: '+380968364957', places: 1 },
-    { start: 'Одеса', end: 'Южноукраїнськ', time_start: '09:00', name_driver: 'Дімка', phone_number: '+380668362967', places: 4 },
-  ])
+function HomeScreen({ navigation }) {
   return (
-<SafeAreaView style={styles.container}>
-      <LinearGradient
-        style={styles.container_main}
-        colors={["#777CE3", "#49EDBC", "#00FFF0"]}
-        start={{ x: 0.1, y: 0.6 }}
-        end={{ x: 0.9, y: 0.4 }}>
-        
-        <Header text={"Рейси"} />
-
-      <FlatList data={listOfItems} renderItem={({ item }) => (
-          <ListJourney el={item}/>
-        )} >
-        </FlatList>
-        
-        <Footer/>
-
-      </LinearGradient>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <View style={{ flex: 1}}>
+      <Inputs/>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+    </View>
+  );
+}
 
-  container_main: {
-    flex: 1,
-  },
-});
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ inputs.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, SafeAreaView, TextInput, StyleSheet } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient';
+
+class Inputs extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
+   render() {
+      return (
+         <SafeAreaView style={styles.container}>
+      <LinearGradient
+        style={styles.container}
+        colors={["#F2F491", "#8B93DF"]}
+        start={{ x: 0, y: 1 }}
+        end={{ x: 0, y: 0 }}
+        //locations={[0.1, 0.3]}
+      >
+         <View style = {styles.inputcontainer}>
+            <Text style={styles.Header}>Я волонтер</Text>
+            <Text style={styles.inputtext}>ПІБ:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Email"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+            <Text style={styles.inputtext}>Номер телефону:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <Text style={styles.inputtext}>Адреса відправлення:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <Text style={styles.inputtext}>Адреса прибуття:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <Text style={styles.inputtext}>Дата і час відправлення:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <Text style={styles.inputtext}>Кількість місць в авто:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <Text style={styles.inputtext}>Опис авто:</Text>
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+            //    placeholder = "Password"
+            //    placeholderTextColor = "#2D2C34"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+         </View>
+         
+       </LinearGradient>
+    </SafeAreaView>
+      )
+   }
+}
+export default Inputs
+
+const styles = StyleSheet.create({
+   container: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: -200,
+    zIndex: -1
+    },
+    inputtext: {
+        fontFamily: 'Montserrat',
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: 20,
+        lineHeight: 24,
+    },
+   input: {
+      marginBottom: 20,
+      height: 32,
+      borderColor: '#2D2C34',
+      borderWidth: 1,
+      width: 275,
+      color: '#1B1D1F',
+      backgroundColor: '#F3FFFE',
+      fontFamily: 'Montserrat',
+      fontStyle: 'normal',
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: 62,
+   },
+   submitButton: {
+      backgroundColor: '#2D2C34',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   },
+   Header:{
+    color: '#1B1D1F',
+    fontFamily: 'Montserrat',
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: 28,
+    lineHeight: 72,
+
+    left: 28,
+    top: 28,
+    width: 180,
+    height: 36,
+    display: 'flex',
+    alignItems: 'center',
+    position: 'absolute',
+   },
+   inputcontainer:{
+    paddingHorizontal: 50,
+    paddingTop: 150,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: 3,
+    width: 275,
+    height: 62,
+    
+   },
+})
